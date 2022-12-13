75b5df57ab7521739413e78ce6b8e7aba1e30713
DrcWinata <federic.winata@binus.ac.id>
2022-12-13 16:40:56 +0700

    tes

--- app/src/main/java/com/example/homespace/MainActivity.java
+++ app/src/main/java/com/example/homespace/MainActivity.java
@@ -13,7 +13,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     String name;
-
+// tes tes
     //    TextView username;
     TextView viewDate;
     Calendar calendar;
