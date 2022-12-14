61226b1420552de3c2a8af789d33bb1047e936cb
Stvn07 <steven123ng@gmail.com>
2022-12-14 15:25:51 +0700

    spinner belum jadi

---
+++ app/src/main/java/com/example/homespace/HistoryActivity.java
@@ -0,0 +1,36 @@
+package com.example.homespace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class HistoryActivity extends AppCompatActivity {
+Spinner spinner;
+String[] category ={"Home Supplies", "Food Ingredients", "Snacks" , "Health Items", "Drinks"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+        spinner.findViewById(R.id.spinner);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(HistoryActivity.this, android.R.layout.simple_spinner_item,category);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
+
+    }
+
+
+}
